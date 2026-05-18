1ec5a3434cef6273dbd4436ad7ae8bac7c484818
ryankeefe92 <ryantkeefe@gmail.com>
2026-05-17 21:16:47 -0400

    Initial Commit

---
+++ Torrent MatchTests/Torrent_MatchTests.swift
@@ -0,0 +1,18 @@
+//
+//  Torrent_MatchTests.swift
+//  Torrent MatchTests
+//
+//  Created by Ryan Keefe on 5/17/26.
+//
+
+import Testing
+
+struct Torrent_MatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
